262c606fc7d49d1a1aa907d69bb7cf4fe3de5492
Ellipsis <rizkyfathur326@gmail.com>
2025-05-23 15:44:21 +0700

    add seed script to initialize database and seed application details from data directory

---
+++ src/seed.py
@@ -0,0 +1,18 @@
+from backend.database_manager import DatabaseManager
+from backend.seeder import Seeder
+from backend.core_manager import BackendManager
+
+if __name__ == '__main__':
+
+    db_manager = DatabaseManager(
+        host='localhost', user='root', password='', db='ats_db')
+    db_manager.connect()
+
+    db_manager.create_tables()
+
+    data_dir = '../data/'
+
+    seeder = Seeder(db_manager)
+    seeder.seed_application_details_from_paths(data_dir)
+
+    db_manager.close()
